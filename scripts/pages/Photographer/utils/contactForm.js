739a4d09f5d2597ee41f372031997842259641c1
Matheus Cristofoli <matfrietasc@gmail.com>
2022-08-22 02:07:15 -0400

    added simple form validation

--- scripts/pages/Photographer/utils/contactForm.js
+++ scripts/pages/Photographer/utils/contactForm.js
@@ -14,3 +14,25 @@ closeModal.addEventListener('click', () => {
   modal.close();
 });
 export default openModal;
+
+// Dialog on submit form
+const form = document.getElementById('modal');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (
+    firstName.value === '' &&
+    lastName.value == '' &&
+    email.value === '' &&
+    message.value === ''
+  ) {
+    alert('Please fill in all fields');
+  } else {
+    console.log('Form submitted');
+    modal.close();
+  }
+});
