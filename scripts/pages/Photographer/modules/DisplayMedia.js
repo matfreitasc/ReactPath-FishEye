d1fdd46bdabe2e618e8692a8dcb7d5bdd081be5b
Matheus Cristofoli <matfrietasc@gmail.com>
2022-08-26 00:06:38 -0400

    Fixed the LikeButton, removed unique id

--- scripts/pages/Photographer/modules/DisplayMedia.js
+++ scripts/pages/Photographer/modules/DisplayMedia.js
@@ -31,7 +31,7 @@ export default function displayMedia() {
                   <h2 class="gallery-item-title" aria-label="${profileMedia.title}">${profileMedia.title}</h2>
                   <div class="gallery-item-likes">
                   <span aria-label="${profileMedia.likes} Likes " id='likesAmmount'>${profileMedia.likes}</span>
-                  <button  aria-label="like" id="likeButton${profileMedia.id}" >
+                  <button  aria-label="like" id="likeButton" >
                   <svg  xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
                   <path data-mediaId="${profileMedia.id}" fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                   </svg>
